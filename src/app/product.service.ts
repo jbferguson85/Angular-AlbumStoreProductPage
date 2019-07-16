60a274f79c670ac59f6095b485fc347b286667ec
JB Ferguson <joseph.ferguson@dtn.com>
2019-07-16 14:03:25 -0500

    First crack at a product list.

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -3,6 +3,7 @@ import { Http, Response } from '@angular/http';
 import 'rxjs/add/operator/map';
 import { Observable } from 'rxjs/Observable';
 import { Album } from './album';
+import { Product } from './product';
 
 
 
@@ -10,10 +11,15 @@ import { Album } from './album';
 @Injectable()
 export class ProductService {
   private _albumUrl = '../assets/album.json';
+  private _productsUrl = '../assets/products.json';
 
   constructor(private _http: Http) { }
 
   getAlbum(id: number): Observable<Album> {
     return this._http.get(this._albumUrl).map((response) => <Album>response.json());
+  }
+
+  getProducts(): Observable<Product[]> {
+    return this._http.get(this._productsUrl).map((response) => <Product[]>response.json());
   }
 }
